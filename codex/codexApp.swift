760d7310e93cb6c6b6ad7d69fbc1b45b95634df7
Lawrence D'Anna <larry@elder-gods.org>
2026-05-12 12:10:59 -0400

    Initial Commit

---
+++ codex/codexApp.swift
@@ -0,0 +1,17 @@
+//
+//  codexApp.swift
+//  codex
+//
+//  Created by Lawrence D'Anna on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct codexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
